ab93ef7590480024175560c780b649ba62f1b493
Ben Sheesley <ben@axismaps.com>
2021-07-21 14:58:47 -0500

    Location property for initial chapters

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {
     style: 'mapbox://styles/bcsheesley/ckrclicp410k317pc4fdmfh71',
     accessToken: 'pk.eyJ1IjoiYmNzaGVlc2xleSIsImEiOiJja3JiNXY5ZDIwMTI0MnFwZnNvYTVuZHN5In0.X4Nj3sDbgu-1yAM_T8ymUg',
-    showMarkers: true,
+    showMarkers: false,
     markerColor: '#3FB1CE',
     theme: 'light',
     use3dTerrain: false,
@@ -18,10 +18,10 @@ var config = {
             image: './path/to/image/source.png',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             location: {
-                center: [-122.418398, 37.759483],
-                zoom: 8.5,
-                pitch: 60,
-                bearing: 0
+                center: [1.17528, 52.23786],
+                zoom: 12,
+                pitch: 80,
+                bearing: 102
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
@@ -48,10 +48,10 @@ var config = {
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
-                center: [-77.020636, 38.886900],
-                zoom: 8.5,
-                pitch: 60,
-                bearing: -43.2
+                center: [13.39272, 52.51166],
+                zoom: 12,
+                pitch: 80,
+                bearing: 102
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
