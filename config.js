6f711b27b65658cecd7227c8fb74df22eab999e3
Ben Sheesley <ben@axismaps.com>
2021-08-04 10:52:12 -0500

    Add test image

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: 'Lt. B.E. Sheesley',
-            image: './path/to/image/source.png',
+            image: './images/osm.png',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             location: {
                 center: [10.05168, 51.11306],
