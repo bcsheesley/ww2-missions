000eb25317e9fa4019b208ff1424140a37074d9e
Ben Sheesley <ben@axismaps.com>
2021-08-09 15:37:16 -0500

    Hacked together page navigation menu

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ var config = {
             id: 'overview-1',
             alignment: 'left',
             hidden: false,
-            title: ' ',
+            title: 'Introduction',
             image: './images/id-card-front.jpeg',
             imageId: 'idCardFront',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
