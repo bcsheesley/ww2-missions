3234dab1801ae6653fe67de5a74fcb4fc10d8ad9
Ben Sheesley <ben@axismaps.com>
2021-09-30 23:09:04 -0500

    Add HTML markers

---
+++ markers.js
@@ -0,0 +1,653 @@
+const markers = {
+"type": "FeatureCollection",
+"features": [
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				1.07976,
+				52.249163,
+				0
+			]
+		},
+		"properties": {
+			"name": "Mendlesham",
+			"date": "",
+			"mission": "",
+			"daterank": "",
+			"trips": ""
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				1.07976,
+				52.249163,
+				0
+			]
+		},
+		"properties": {
+			"name": "Recall",
+			"date": "3/4/1945",
+			"mission": "136",
+			"daterank": "22",
+			"trips": "1"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				13.404954,
+				52.5200066,
+				0
+			]
+		},
+		"properties": {
+			"name": "Berlin",
+			"date": "12/5/1944",
+			"mission": "97",
+			"daterank": "1",
+			"trips": "4"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				13.404954,
+				52.5200066,
+				0
+			]
+		},
+		"properties": {
+			"name": "Berlin",
+			"date": "2/3/1945",
+			"mission": "119",
+			"daterank": "14",
+			"trips": "4"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				13.404954,
+				52.5200066,
+				0
+			]
+		},
+		"properties": {
+			"name": "Berlin",
+			"date": "2/26/1945",
+			"mission": "131",
+			"daterank": "19",
+			"trips": "4"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				13.404954,
+				52.5200066,
+				0
+			]
+		},
+		"properties": {
+			"name": "Berlin",
+			"date": "3/18/1945",
+			"mission": "145",
+			"daterank": "28",
+			"trips": "4"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				8.6511929,
+				49.8728253,
+				0
+			]
+		},
+		"properties": {
+			"name": "Darmstadt",
+			"date": "12/12/1944",
+			"mission": "100",
+			"daterank": "2",
+			"trips": "1"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				9.7320104,
+				52.3758916,
+				0
+			]
+		},
+		"properties": {
+			"name": "Hanover",
+			"date": "12/15/1944",
+			"mission": "101",
+			"daterank": "3",
+			"trips": "2"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				6.9609652,
+				50.937531,
+				0
+			]
+		},
+		"properties": {
+			"name": "Cologne",
+			"date": "1/10/1945",
+			"mission": "110",
+			"daterank": "9",
+			"trips": "1"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				9.7320104,
+				52.3758916,
+				0
+			]
+		},
+		"properties": {
+			"name": "Hanover",
+			"date": "3/28/1945",
+			"mission": "153",
+			"daterank": "35",
+			"trips": "2"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				7.4086636,
+				50.426057,
+				0
+			]
+		},
+		"properties": {
+			"name": "Andernach",
+			"date": "12/27/1944",
+			"mission": "103",
+			"daterank": "4",
+			"trips": "1"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				9.4797461,
+				51.3127114,
+				0
+			]
+		},
+		"properties": {
+			"name": "Kassel",
+			"date": "12/30/1944",
+			"mission": "105",
+			"daterank": "5",
+			"trips": "1"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				9.851202,
+				52.390962,
+				0
+			]
+		},
+		"properties": {
+			"name": "Misburg",
+			"date": "12/31/1944",
+			"mission": "106",
+			"daterank": "6",
+			"trips": "1"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				8.6946286,
+				48.8921862,
+				0
+			]
+		},
+		"properties": {
+			"name": "Pforzheim",
+			"date": "1/3/1945",
+			"mission": "108",
+			"daterank": "7",
+			"trips": "1"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				8.5324708,
+				52.0302285,
+				0
+			]
+		},
+		"properties": {
+			"name": "Bielefeld",
+			"date": "1/7/1945",
+			"mission": "109",
+			"daterank": "8",
+			"trips": "1"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				11.9977096,
+				52.4165503,
+				0
+			]
+		},
+		"properties": {
+			"name": "Derben",
+			"date": "1/14/1945",
+			"mission": "112",
+			"daterank": "10",
+			"trips": "1"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				10.89779,
+				48.3705449,
+				0
+			]
+		},
+		"properties": {
+			"name": "Augsburg",
+			"date": "1/15/1945",
+			"mission": "113",
+			"daterank": "11",
+			"trips": "1"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				12.3133061,
+				51.6265334,
+				0
+			]
+		},
+		"properties": {
+			"name": "Bitterfeld",
+			"date": "1/16/1945",
+			"mission": "114",
+			"daterank": "12",
+			"trips": "2"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				12.3133061,
+				51.6265334,
+				0
+			]
+		},
+		"properties": {
+			"name": "Bitterfeld",
+			"date": "3/17/1945",
+			"mission": "144",
+			"daterank": "27",
+			"trips": "2"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				6.7623293,
+				51.4344079,
+				0
+			]
+		},
+		"properties": {
+			"name": "Duisburg",
+			"date": "1/28/1945",
+			"mission": "117",
+			"daterank": "13",
+			"trips": "1"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				7.2834926,
+				51.8316451,
+				0
+			]
+		},
+		"properties": {
+			"name": "Dülmen",
+			"date": "2/9/1945",
+			"mission": "121",
+			"daterank": "15",
+			"trips": "1"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				14.3328679,
+				51.7563108,
+				0
+			]
+		},
+		"properties": {
+			"name": "Cottbus",
+			"date": "2/15/1945",
+			"mission": "123",
+			"daterank": "16",
+			"trips": "1"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				8.8016937,
+				53.0792962,
+				0
+			]
+		},
+		"properties": {
+			"name": "Bremen",
+			"date": "2/24/1945",
+			"mission": "129",
+			"daterank": "17",
+			"trips": "1"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				13.261627,
+				52.3011439,
+				0
+			]
+		},
+		"properties": {
+			"name": "Ludwigsfelde",
+			"date": "2/25/1945",
+			"mission": "130",
+			"daterank": "18",
+			"trips": "1"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				13.7372621,
+				51.0504088,
+				0
+			]
+		},
+		"properties": {
+			"name": "Dresden",
+			"date": "3/2/1945",
+			"mission": "134",
+			"daterank": "20",
+			"trips": "1"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				10.2291851,
+				52.432764,
+				0
+			]
+		},
+		"properties": {
+			"name": "Dedenhausen",
+			"date": "3/3/1945",
+			"mission": "135",
+			"daterank": "21",
+			"trips": "1"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				7.3348301,
+				51.6461362,
+				0
+			]
+		},
+		"properties": {
+			"name": "Datteln",
+			"date": "3/7/1945",
+			"mission": "137",
+			"daterank": "23",
+			"trips": "1"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				8.105754,
+				51.5711476,
+				0
+			]
+		},
+		"properties": {
+			"name": "Soest",
+			"date": "3/10/1945",
+			"mission": "139",
+			"daterank": "24",
+			"trips": "1"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				9.9936818,
+				53.5510846,
+				0
+			]
+		},
+		"properties": {
+			"name": "Hamburg",
+			"date": "3/11/1945",
+			"mission": "140",
+			"daterank": "25",
+			"trips": "2"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				9.9936818,
+				53.5510846,
+				0
+			]
+		},
+		"properties": {
+			"name": "Hamburg",
+			"date": "3/20/1945",
+			"mission": "147",
+			"daterank": "30",
+			"trips": "2"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				14.2475775,
+				53.9100327,
+				0
+			]
+		},
+		"properties": {
+			"name": "Świnoujście",
+			"date": "3/12/1945",
+			"mission": "141",
+			"daterank": "26",
+			"trips": "1"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				11.5892372,
+				50.927054,
+				0
+			]
+		},
+		"properties": {
+			"name": "Jena",
+			"date": "3/19/1945",
+			"mission": "146",
+			"daterank": "29",
+			"trips": "1"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				10.0010767,
+				53.3105751,
+				0
+			]
+		},
+		"properties": {
+			"name": "Marxen",
+			"date": "3/12/1945",
+			"mission": "148",
+			"daterank": "31",
+			"trips": "1"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				6.8401844,
+				51.2964148,
+				0
+			]
+		},
+		"properties": {
+			"name": "Ratingen",
+			"date": "3/22/1945",
+			"mission": "149",
+			"daterank": "32",
+			"trips": "1"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				7.6194012,
+				51.4584411,
+				0
+			]
+		},
+		"properties": {
+			"name": "Geisecke",
+			"date": "3/23/1945",
+			"mission": "150",
+			"daterank": "33",
+			"trips": "1"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				8.0098765,
+				53.1814401,
+				0
+			]
+		},
+		"properties": {
+			"name": "Bad Zwischenahn",
+			"date": "3/24/1945",
+			"mission": "151",
+			"daterank": "34",
+			"trips": "1"
+		}
+	}
+]
+};
