68bed3bb00ad73addacae1d39d32a00b27ea20d9
Ben Sheesley <ben@axismaps.com>
2021-10-18 11:58:56 -0500

    Remove point popup

--- popup.js
+++ popup.js
@@ -73,69 +73,6 @@ map.on('click', 'routesPopup', (e) => {
 	.addTo(map);
 });
 
-// Mission points hover popup
-map.on('mouseenter', 'missions', (e) => {
-	map.getCanvas().style.cursor = 'pointer';
-	
-	const name = e.features[0].properties.name;
-
-	popupHover
-	.setHTML(`<h3>${name}</h3><p><i>Click for more details...</i></p>`)
-	.addTo(map)
-	.setLngLat(e.lngLat)
-	.trackPointer();
-});
-
-map.on('mouseleave', 'missions', () => {
-	map.getCanvas().style.cursor = '';
-	popupHover.remove();
-});
-
-// Mission points click popup
-map.on('click', 'missions', (e) => {
-	const name = e.features[0].properties.name;
-	const daterank1 = e.features[0].properties.daterank1||'';
-	const date1 = e.features[0].properties.date1||'';
-	const daterank2 = e.features[0].properties.daterank2||'';
-	const date2 = e.features[0].properties.date2||'';
-	const daterank3 = e.features[0].properties.daterank3||'';
-	const date3 = e.features[0].properties.date3||'';
-	const daterank4 = e.features[0].properties.daterank4||'';
-	const date4 = e.features[0].properties.date4||'';
-	
-	popupClick
-	.setLngLat(e.lngLat)
-	.setHTML(
-		`<h3>${name}</h3>
-		<table>
-			<tr>
-				<th>Mission</th>
-				<th>Date</th>
-			</tr>
-			<tr>
-				<td>${daterank1}</td>
-				<td><a href=#journal>${date1}</a></td>
-			</tr>
-			<tr>
-				<td>${daterank2}</td>
-				<td><a href=#journal>${date2}</a></td>
-			</tr>
-			<tr>
-				<td>${daterank3}</td>
-				<td><a href=#journal>${date3}</a></td>
-			</tr>
-			<tr>
-				<td>${daterank4}</td>
-				<td><a href=#journal>${date4}</a></td>
-			</tr>
-		</table>
-		<p>
-			<i>Lt. Sheesley flew 35 combat missions in total.</i>
-		</p>`
-	)
-	.addTo(map);
-});
-
 // Service schools hover popup
 map.on('mouseenter', 'serviceSchools', (e) => {
 	map.getCanvas().style.cursor = 'pointer';
