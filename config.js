1b20b290629fdc629f09af1654b15c114766857f
Ben Sheesley <ben@axismaps.com>
2021-10-26 11:47:01 -0500

    Drop caps and drop quotes

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ var config = {
             date: '',
             image: './images/id-card-front-small.png',
             imageId: 'idCardFront',
-            description: 'Family from <span class= "test">Struthers, OH, near Youngstown</span>. Trains to become a pilot at several bases in US: Walnut Springs, Ardmore Airforce, and others. 2nd Lt. Later becomes 1st Lt. Starts preparing in the fields of Arkansas',
+            description: '<p><span class="dropCap">F</span>amily from <span class= "test">Struthers, OH, near Youngstown</span>. Trains to become a pilot at several bases in US: Walnut Springs, Ardmore Airforce, and others. 2nd Lt. Later becomes 1st Lt. Starts preparing in the fields of Arkansas</p>',
             location: {
                 center: [-90.95484, 36.06806],
                 zoom: 4,
@@ -43,7 +43,7 @@ var config = {
             date: '',
             image: './images/he_Dominion_Monarch_at_a_wharf_in_Wellington,_1940s_(3377606529).jpg',
             imageId: 'idCardFront',
-            description: 'Departs from NYC Harbor aboard ship. Writes in another journal. Arrives in [port city], England after a week at sea. Travels by train to fields of Mendlesham, England.',
+            description: '<p><span class="dropCap">D</span>eparts from NYC Harbor aboard ship. Writes in another journal. Arrives in [port city], England after a week at sea. Travels by train to fields of Mendlesham, England.</p>',
             location: {
                 center: [-39.43332, 44.67845],
                 zoom: 2,
@@ -78,12 +78,12 @@ var config = {
             date: '',
             image: './images/Mendelshamairfield-18jan47.png',
             imageId: 'missions',
-            description: '8th Airforce, 34th bomb group, 7th squadron. Other groups were also present before and during. Spends about four months total here. Plays basketball, goes to London, sees a play, attends church, receives and sends letters (?), ride a bike, and flies missions',
+            description: '<p><span class="dropCap">8</span>th Airforce, 34th bomb group, 7th squadron. Other groups were also present before and during. Spends about four months total here. Plays basketball, goes to London, sees a play, attends church, receives and sends letters (?), ride a bike, and flies missions</p>',
             location: {
                 center: [1.1210006854646852, 52.23269925011281],
                 zoom: 14,
                 pitch: 0,
-                bearing: 0
+                bearing: 16
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
@@ -101,7 +101,7 @@ var config = {
             date: '',
             image: './images/missions-card-small.png',
             imageId: 'idCardFront',
-            description: 'Flies 35 missions over Germany. Some short, some long trips. Some repeated targets. Some taregets not hit. An accident. A recall.',
+            description: '<p><span class="dropCap">F</span>lies 35 missions over Germany. Some short, some long trips. Some repeated targets. Some taregets not hit. An accident. A recall.</p>',
             location: {
                 center: [1.17528, 52.23786],
                 zoom: 5,
@@ -124,7 +124,7 @@ var config = {
             date: '',
             image: './images/journal/front-cover-1.png',
             imageId: 'idCardFront',
-            description: 'Begins with a surprise mission, battles weather, cold, exhaustion, equipment, oxygen, nerves, flack, uncertainty, longing for Mary. Experiences scary moments and moments of comfort in the sky. Receives honors (but never recorded in these pages).',
+            description: '<p><span class="dropCap">B</span>egins with a surprise mission, battles weather, cold, exhaustion, equipment, oxygen, nerves, flack, uncertainty, longing for Mary. Experiences scary moments and moments of comfort in the sky. Receives honors (but never recorded in these pages).</p>',
             location: {
                 center: [1.17528, 52.23786],
                 zoom: 5,
@@ -147,7 +147,7 @@ var config = {
             date: '12/5/1944, p. 1',
             image: './images/journal/19441205-1-trim.png',
             imageId: 'dec5a',
-            description: '<p>I was awakened at 0400 expecting to ride as copilot for my 1st mission. At the briefing block I was surprised, alarmed and frightened when I learned that we would go as a crew and I didn\'t know the score at all and had never flown above 20,000 ft. In a daze we went to our ship and somehow got ready and took off. After what seemed ages the formation got together in a pre-dawn assembly and away we went for BERLIN.</p><p>On the climb we stayed right in. Then we leveled off for the run in and it happened. That which is most disastrous happened—we fell behind. Opening her wide open we oh! so slowly crept back up. Come the IP and it happened again. I took over the ship again—the bombays were open—target just ahead—we had to get back in! If fighters hit we would have been sunk.</p><p>All our hearts were pounding—nerves at the break point-sweat rolling at 40° below. I quickly turned on the emergency turbo and the engines were giving all they had—they shook and shuddered and whined as if to say "we can\'t take this long." We were pulling 54" and I wondered how long those engines would hold. We just didn\'t seem to gain a bit, but kept falling back & dropping.</p><p>Finally we were down with the low element when we stopped losing. I uttered a little prayer and as a plant grows...</p>',
+            description: '<p><span class="dropQuote">&ldquo;</span>I was awakened at 0400 expecting to ride as copilot for my 1st mission. At the briefing block I was surprised, alarmed and frightened when I learned that we would go as a crew and I didn\'t know the score at all and had never flown above 20,000 ft. In a daze we went to our ship and somehow got ready and took off. After what seemed ages the formation got together in a pre-dawn assembly and away we went for BERLIN.</p><p>On the climb we stayed right in. Then we leveled off for the run in and it happened. That which is most disastrous happened—we fell behind. Opening her wide open we oh! so slowly crept back up. Come the IP and it happened again. I took over the ship again—the bombays were open—target just ahead—we had to get back in! If fighters hit we would have been sunk.</p><p>All our hearts were pounding—nerves at the break point-sweat rolling at 40° below. I quickly turned on the emergency turbo and the engines were giving all they had—they shook and shuddered and whined as if to say "we can\'t take this long." We were pulling 54" and I wondered how long those engines would hold. We just didn\'t seem to gain a bit, but kept falling back & dropping.</p><p>Finally we were down with the low element when we stopped losing. I uttered a little prayer and as a plant grows [...]</p>',
             location: {
                 center: [13.39272, 52.51166],
                 zoom: 10,
@@ -176,7 +176,7 @@ var config = {
             date: '12/5/1944, p. 2',
             image: './images/journal/19441205-2.jpeg',
             imageId: 'dec5b',
-            description: '<p>...the airspeed moved to 140, then 141, then 142; it seemed to stop and my heart seemed to stop then it moved to 145 and we began to move up again. Carefully I lifted her up as we gained on our lead. Just as we dropped the bombs we got back in place, but low.</p><p>That was it—the quick turn to escape flack and we were homeward bound. The 10 minutes from the IP to the target were the longest and most tense, exhausting ones of my life. At the Rally point I looked down just in time to see a lone B-17 spin down through the clouds below.</p><p>When we landed the squadron C.O. and all were there to meet us. Then we realized that, as I suspected, we had been sent by mistake—a green crew. They didn\'t think we would make it—we weren\'t even operational yet. "We had it!" Brydge was so frightened he made only 3 entries in his log: "(1) took off (2) Bombs away (3) landed." The hot cocoa sure tasted good. Next day we went on pass and sure needed it!</p>',
+            description: '<p><span class="dropQuote">&ldquo;</span>the airspeed moved to 140, then 141, then 142; it seemed to stop and my heart seemed to stop then it moved to 145 and we began to move up again. Carefully I lifted her up as we gained on our lead. Just as we dropped the bombs we got back in place, but low.</p><p>That was it—the quick turn to escape flack and we were homeward bound. The 10 minutes from the IP to the target were the longest and most tense, exhausting ones of my life. At the Rally point I looked down just in time to see a lone B-17 spin down through the clouds below.</p><p>When we landed the squadron C.O. and all were there to meet us. Then we realized that, as I suspected, we had been sent by mistake—a green crew. They didn\'t think we would make it—we weren\'t even operational yet. "We had it!" Brydge was so frightened he made only 3 entries in his log: "(1) took off (2) Bombs away (3) landed." The hot cocoa sure tasted good. Next day we went on pass and sure needed it!</p>',
             location: {
                 center: [13.39272, 52.51166],
                 zoom: 10,
@@ -205,7 +205,7 @@ var config = {
             date: '12/30/1944',
             image: './images/journal/19441230.jpeg',
             imageId: 'dec30',
-            description: 'Briefing this morning was at 04:00. Our target for today was a marshalling yard at [crossed out] Castle [end crossed out] Kassel, Germany. We took off and hit a low ceiling and climbed in solid overcast up to 11,000 ft. Our position was 4–3 in High squadron. Overcast 10/10 over target—results not observed. Flack was dense and we got our first hits. Our gas tank in right wing had to be replaced upon return. Oxygen system was leaking and was empty as we reached 10,000\' on descending—sweating it out. Visibility very poor upon return—we made 6 passes at field before getting in. Went to bed at 20:00 hours.',
+            description: '<p><span class="dropQuote">&ldquo;</span>Briefing this morning was at 04:00. Our target for today was a marshalling yard at <s>Castle</s> Kassel, Germany. We took off and hit a low ceiling and climbed in solid overcast up to 11,000 ft. Our position was 4–3 in High squadron. Overcast 10/10 over target—results not observed. Flack was dense and we got our first hits. Our gas tank in right wing had to be replaced upon return. Oxygen system was leaking and was empty as we reached 10,000\' on descending—sweating it out. Visibility very poor upon return—we made 6 passes at field before getting in. Went to bed at 20:00 hours.</p>',
             location: {
                 center: [9.4797461, 51.3127114],
                 zoom: 10,
