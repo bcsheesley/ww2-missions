4f78e121dc5cf7f55b326a5895fed747abfce46b
Ben Sheesley <ben@axismaps.com>
2021-12-10 11:52:26 -0600

    Split out locations into their own file

---
+++ locations.js
@@ -0,0 +1,73 @@
+// Struthers, OH
+var struthers = [-80.60282, 41.05850];
+
+// Mendlesham
+var mendlesham = [1.121001, 52.232699];
+
+// RAF Marston Moor
+var marstonmoor = [-1.2989809701184143, 53.96346655488253];
+
+// Kilmer
+var kilmer = [-74.4458, 40.5167];
+
+// New York Harbor
+var newyork = [-74.0460, 40.6683];
+
+// At sea start
+var sea1 = [-70.6691, 41.1097];
+
+// At sea middle
+var sea2 = [-66.49730984863106, 43.09594080607482];
+var sea3 = [-60.18472272639963, 45.59582421709126];
+var sea4 = [-53.63687116389963, 47.61823809916669];
+var sea5 = [-46.869293038899514, 49.25081403213443];
+var sea6 = [-40.277496163899514, 50.328843938377474];
+var sea7 = [-33.20230085139934, 51.10787025644228];
+var sea8 = [-25.951324288899173, 51.382960620270325];
+var sea9 = [-18.78823835139906, 51.24562111169806];
+var sea10 = [-14.360748117024855, 50.935102571605825];
+
+// At sea end
+var sea11 = [-8.382820088197718, 50.241322780820326];
+
+// Plymouth Bay
+var plymouthBay = [-4.156, 50.3266];
+
+// Plymouth Docks
+var plymouthDock = [-4.1556, 50.3640];
+
+//Yarnfield Park
+var yarnfield = [-2.1990963667560663, 52.895682841527304];
+ 
+// Destinations
+var berlin = [13.404954, 52.520006];
+var darmstadt = [8.651193, 49.872825];
+var hanover = [9.732010, 52.375892];
+var andernach = [7.408664, 50.42606];
+var kassel = [9.479746, 51.312711];
+//var kassel = [9.480, 51.312];
+var misburg = [9.851202, 52.390962];
+//var misburg = [9.851, 52.391];
+var pforzheim = [8.694629, 48.892186];
+var bielefeld = [8.532471, 52.030229];
+var cologne = [6.960965, 50.93753];
+var derben = [11.997710, 52.416550];
+var augsburg = [10.89779, 48.370545];
+var bitterfeld = [12.313306, 51.626533];
+var duisburg = [6.762329, 51.434408];
+var dulmen = [7.283493, 51.831645];
+var cottbus = [14.334167, 51.760556];
+var bremen = [8.801694, 53.079296];
+var ludwigsfelde = [13.266667, 52.299722];
+var dresden = [13.737262, 51.050409];
+var dedenhausen = [10.229185, 52.43276];
+var datteln = [7.334830, 51.646136];
+var soest = [8.10575, 51.571148];
+var hamburg = [9.993682, 53.551085];
+var swinoujscie = [14.247578, 53.910033];
+var jena = [11.589237, 50.92705];
+var marxen = [10.001077, 53.310575];
+var ratingen = [6.840184, 51.296415];
+var geisecke = [7.619401, 51.458441];
+var badZwischenahn = [8.009877, 53.181440];
+var recall = [6.605980, 49.311541];
